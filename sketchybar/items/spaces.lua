975c03ca3a3dcf6b8b888d72408453f275b9ad24
Frail Bongat <frail.bongat@gmail.com>
2024-12-01 20:34:45 +0800

    wip: hide ongoing space changes

--- sketchybar/items/spaces.lua
+++ sketchybar/items/spaces.lua
@@ -42,13 +42,13 @@ for i = 1, 10, 1 do
 end
 
 
-local spaces_bracket = sbar.add("bracket", {
-  'space.1', 'space.2', 'space.3', 'space.4'
-}, {
-  background = {
-    color = colors.red
-  },
-})
+-- local spaces_bracket = sbar.add("bracket", {
+--   'space.1', 'space.2', 'space.3', 'space.4'
+-- }, {
+--   background = {
+--     color = colors.red
+--   },
+-- })
 
 local space_window_observer = sbar.add("item", {
   drawing = false,
