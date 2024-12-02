a9a1321ee3c76e27483aeafb08c14802dd29f9d9
Frail Bongat <frail.bongat@gmail.com>
2024-12-02 23:18:32 +0800

    style: make spaces 1 bar

--- sketchybar/items/spaces.lua
+++ sketchybar/items/spaces.lua
@@ -17,15 +17,26 @@ for i = 1, 10, 1 do
     label = {
       color = colors.grey,
       highlight_color = colors.white,
-      font = "sketchybar-app-font:Regular:12.0",
+      font = "sketchybar-app-font:Regular:11.0",
       y_offset = -1,
     },
     background = {
-      padding_right = 5
+      color = colors.bg1,
+      padding_right = 8
     },
   })
 
   spaces[i] = space
+
+    -- Padding space
+    sbar.add("space", "space.padding." .. i, {
+      space = i,
+      script = "",
+      width = 5,
+      background = {
+        color = colors.transparent
+      }
+    })
 
   space:subscribe("space_change", function(env)
     local selected = env.SELECTED == "true"
@@ -41,14 +52,13 @@ for i = 1, 10, 1 do
   end)
 end
 
-
--- local spaces_bracket = sbar.add("bracket", {
---   'space.1', 'space.2', 'space.3', 'space.4'
--- }, {
---   background = {
---     color = colors.red
---   },
--- })
+local spaces_bracket = sbar.add("bracket", {
+  'space.1', 'space.2', 'space.3', 'space.4', 'space.5', 'space.6', 'space.7', 'space.8', 'space.9', 'space.10'
+}, {
+  background = {
+    color = colors.bg1
+  },
+})
 
 local space_window_observer = sbar.add("item", {
   drawing = false,
