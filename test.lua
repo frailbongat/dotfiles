68f69d179802dc8ffeab80d72a2940920dcd84b3
Frail Bongat <frail.bongat@icloud.com>
2024-12-04 13:29:41 +0800

    test: add function for checking if battery exist

--- test.lua
+++ test.lua
@@ -15,3 +15,24 @@ function checkDeviceType()
 end
 
 print("You are using: " .. checkDeviceType())
+
+-- Function to check if the Mac device has a battery
+function hasBattery()
+  local handle = io.popen("system_profiler SPPowerDataType | grep 'Battery Information'")
+  local result = handle:read("*a")
+  handle:close()
+
+  -- Check for battery presence
+  if result == "" then
+    return false
+  else
+    return true
+  end
+end
+
+-- Example usage
+if hasBattery() then
+  print("This device has a battery.")
+else
+  print("This device does not have a battery.")
+end
