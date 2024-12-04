c483034d729e7b4374613d11bfd06b54bc172809
Frail Bongat <frail.bongat@icloud.com>
2024-12-04 13:33:54 +0800

    feat: hide battery when device has no battery

--- sketchybar/items/widgets/init.lua
+++ sketchybar/items/widgets/init.lua
@@ -1,4 +1,21 @@
-require("items.widgets.battery")
+-- Function to check if the Mac device has a battery
+function hasBattery()
+  local handle = io.popen("system_profiler SPPowerDataType | grep 'Battery Information'")
+  local result = handle:read("*a")
+  handle:close()
+
+  -- Check for battery presence
+  if result == "" then
+    return false
+  else
+    return true
+  end
+end
+
+if hasBattery() then
+  print("This device has a battery.")
+  require("items.widgets.battery")
+end
 require("items.widgets.volume")
 require("items.widgets.wifi")
 require("items.widgets.cpu")
