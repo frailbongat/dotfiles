ce35d5a06d8f2c40db2f8799a5814e91f8c8e620
Frail Bongat <frail.bongat@icloud.com>
2024-12-04 20:33:53 +0800

    style: add icon to calendar item

--- sketchybar/items/calendar.lua
+++ sketchybar/items/calendar.lua
@@ -1,23 +1,49 @@
 local settings = require("settings")
 local colors = require("colors")
 
-local cal = sbar.add("item", {
-  icon = {
-    color = colors.red,
-    padding_right = 0,
-    font = {
-      style = settings.font.style_map["Medium"],
-      size = 12.0,
-    },
-  },
+local cal_time = sbar.add("item", "cal_time", {
   position = "right",
   update_freq = 30,
   background = {
-    padding_left = settings.group_paddings
+    padding_left = 0
   },
-  label = { font = { family = settings.font.numbers } },
+  label = {
+    font = { family = settings.font.numbers },
+    padding_left = 0,
+  },
+  icon = {
+    drawing = false
+  }
 })
 
-cal:subscribe({ "forced", "routine", "system_woke" }, function(env)
-  cal:set({ icon = os.date("%a %b %d"), label = string.gsub(os.date("%I:%M %p"), "^0", "") })
+local cal_date = sbar.add("item", "cal_date", {
+  position = "right",
+  update_freq = 30,
+  icon = {
+    color = colors.red,
+    padding_right = 0,
+    string = "􀧞"
+  },
+})
+
+local cal_bracket = sbar.add("bracket", "calendar.bracket", {
+  cal_time.name,
+  cal_date.name
+}, {
+  background = {
+    color = colors.bg1,
+  },
+})
+
+sbar.add("item", "calendar.padding", {
+  position = "right",
+  width = settings.group_paddings
+})
+
+cal_time:subscribe({ "forced", "routine", "system_woke" }, function(env)
+  cal_time:set({ label = string.gsub(os.date("%I:%M %p"), "^0", "") })
+end)
+
+cal_date:subscribe({ "forced", "routine", "system_woke" }, function(env)
+  cal_date:set({ label = string.gsub(os.date("%a %b %d"), "0(%d)", "%1") })
 end)
